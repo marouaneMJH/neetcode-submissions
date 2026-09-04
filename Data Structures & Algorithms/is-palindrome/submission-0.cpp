b27b89eb72b8482ad6cd0f7b0d899a375f1adad6
Marouane El Moujahid <marwanmoujahid008@gmail.com>
2026-09-04 19:00:26 +0100

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,27 @@
+#define LOWER(c) (std::tolower(static_cast<unsigned char>(c)))
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0,
+            j = s.length() - 1;
+
+        while(1)
+        {
+            while( (i < s.length()) && !isalnum(s[i])) i++;
+
+            while( j>=0  && !isalnum(s[j])) j--;
+
+            if(j<0 && i >= s.length()) return true;
+
+            if
+            (
+                (j<0 && i < s.length()) ||
+                (j>0 && i >= s.length()) ||
+                LOWER(s[i]) != LOWER(s[j])
+            )   return false;
+
+            i++;j--;
+        }
+    }
+};
